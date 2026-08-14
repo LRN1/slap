e2633cdc6f33a02d9a2eb3f12e4c254a95a66ea8
Mikko Olkkonen <mikko.olkkonen@reaktor.com>
2026-08-14 13:21:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    BUMP CACHE VERSION on every deploy (any change to index.html):
    the changed sw.js byte-diff triggers the browser to install the new
    version; old caches are cleared on activate. */
-const CACHE = "slap-v3";
+const CACHE = "slap-v7";
 const ASSETS = [
   "./",
   "./index.html",
